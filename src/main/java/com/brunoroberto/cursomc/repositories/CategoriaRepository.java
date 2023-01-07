d54d002ce27fb793460ead8c1021a08e7e2f0111
Bruno Roberto <brunoroberto.paulino@gmail.com>
2023-01-07 08:49:25 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/brunoroberto/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,11 @@
+package com.brunoroberto.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.brunoroberto.cursomc.domain.Categoria;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
